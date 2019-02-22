d43c77f1f4fd169be532f746a54b769b32a87037
Huming Chen <huming.chen@ericsson.com>
2019-02-22 09:55:26 +0100

    Add templates for faq_repo.
    Change-Id: I9c91d36bbcf78126887511ae6531653b7c86ab09

--- app/routes.py
+++ app/routes.py
@@ -4,11 +4,22 @@
 import sys
 sys.path.insert(0, "/home/ehumche/private/py3-lib/lib/python3.5/site-packages")
 
-# import app variable from app-module
+from flask import render_template
 from app import app
 
 # view-function for mapping url to request-handler
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Huming'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
