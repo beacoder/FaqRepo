92be8144a0b4484c446a8e3138795484f38587a6
bright <bright@gmail.com>
2019-02-26 01:37:41 +0800

    Add database object, migration object and dabase configuration.

--- app/__init__.py
+++ app/__init__.py
@@ -11,10 +11,14 @@ else:
 
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # app is member variable of app-package
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # place at bottom to avoid circular imports
-from app import routes
+from app import routes, models
