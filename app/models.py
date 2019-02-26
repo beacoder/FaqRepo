0bbadd068dba2a82bf8829159669f9fc67fd2a76
bright <bright@gmail.com>
2019-02-27 00:23:55 +0800

    Create migration and database.

--- app/models.py
+++ app/models.py
@@ -7,78 +7,73 @@ if sys.platform == 'darwin':
 else:
     sys.path.insert(0, "/home/ehumche/private/py3-lib/lib/python3.5/site-packages")
 
-from sqlalchemy import Column, Integer, String, Text
-from .database import Base, db_session, init_db
+from app import db
 
-
-class User(Base):
+class User(db.Model):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    email = Column(String(120), unique=True)
-
-    def __init__(self, name=None, email=None):
-        self.name = name
-        self.email = email
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)  # index makes searching efficient
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
 
     def __repr__(self):
-        return '<User %r>' % (self.name)
+        return '<User {}>'.format(self.username)
 
 
-class Question_Answer(Base):
-    __tablename__ = 'question_answers'
+# class Question_Answer(db.Model):
+#     __tablename__ = 'question_answers'
 
-    id = Column(Integer, primary_key=True)
-    question = Column(String(100), unique=True)
-    answer = Column(Text, unique=True)
+#     id = Column(Integer, primary_key=True)
+#     question = Column(String(100), unique=True)
+#     answer = Column(Text, unique=True)
 
-    def __init__(self, question=None, answer=None):
-        self.question = question
-        self.answer = answer
+#     def __init__(self, question=None, answer=None):
+#         self.question = question
+#         self.answer = answer
 
-    def __repr__(self):
-        return '<Question: %r, Answer: %r>' % (self.question) % (self.answer)
-
-
-class Question(Base):
-    __tablename__ = 'questions'
-
-    id = Column(Integer, primary_key=True)
-    question = Column(String(100), unique=True)
-
-    def __init__(self, question=None):
-        self.question = question
-
-    def __repr__(self):
-        return '<Question: %r>' % (self.question)
+#     def __repr__(self):
+#         return '<Question: %r, Answer: %r>' % (self.question) % (self.answer)
 
 
-# create sqlite database
-init_db()
+# class Question(db.Model):
+#     __tablename__ = 'questions'
 
-# operations on models, later, encapsulate into class
-def add_user(name, email):
-    # pdb.set_trace()
-    if name and email:
-        u = User.query.filter(User.name == name).first()
-        if not u:
-            u = User(name, email)
-            db_session.add(u)
-            db_session.commit()
+#     id = Column(Integer, primary_key=True)
+#     question = Column(String(100), unique=True)
 
-def update_user(name, email):
-    # pdb.set_trace()
-    if name and email:
-        u = User.query.filter(User.name == name).first()
-        if u:
-            u.email = email
-            db_session.commit()
+#     def __init__(self, question=None):
+#         self.question = question
 
-def add_question_answer(question, answer):
-    if question and answer:
-        q_a = Question_Answer.query.filter(Question_Answer.question == question).first()
-        if not q_a:
-            q_a = Question_Answer(question, answer)
-            db_session.add(q_a)
-            db_session.commit()
+#     def __repr__(self):
+#         return '<Question: %r>' % (self.question)
+
+
+# # create sqlite database
+# init_db()
+
+# # operations on models, later, encapsulate into class
+# def add_user(name, email):
+#     # pdb.set_trace()
+#     if name and email:
+#         u = User.query.filter(User.name == name).first()
+#         if not u:
+#             u = User(name, email)
+#             db_session.add(u)
+#             db_session.commit()
+
+# def update_user(name, email):
+#     # pdb.set_trace()
+#     if name and email:
+#         u = User.query.filter(User.name == name).first()
+#         if u:
+#             u.email = email
+#             db_session.commit()
+
+# def add_question_answer(question, answer):
+#     if question and answer:
+#         q_a = Question_Answer.query.filter(Question_Answer.question == question).first()
+#         if not q_a:
+#             q_a = Question_Answer(question, answer)
+#             db_session.add(q_a)
+#             db_session.commit()
