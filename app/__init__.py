7f68d6ed490b22a0a6c92a2fb9ae7d3e7e55ee4e
bright <bright@gmail.com>
2019-02-27 01:36:21 +0800

    Add flask_login to manages user logged-in state.

--- app/__init__.py
+++ app/__init__.py
@@ -13,12 +13,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # member variables of app-package
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 # place at bottom to avoid circular imports
 from app import routes, models
