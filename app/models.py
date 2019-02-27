d4f287c112072171b9c77f5c8df81cfc11b2f2e8
bright <bright@gmail.com>
2019-02-28 01:13:43 +0800

    Prepare user model for flask_login and log users in.

--- app/models.py
+++ app/models.py
@@ -10,8 +10,10 @@ else:
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
+from app import login
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):  # enable flask_login with UserMixin
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -28,6 +30,12 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+# flask_login use this to load user into memory
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class Post(db.Model):
