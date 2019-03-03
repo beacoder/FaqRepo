2170a21fca34287c1311ea8c15d9dd7ec2b6b91e
bright <bright@gmail.com>
2019-03-03 17:25:13 +0800

    Add user registeration functionality.

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,8 @@ else:
     sys.path.insert(0, "/home/ehumche/private/py3-lib/lib/python3.5/site-packages")
 
 from flask import render_template, flash, redirect, url_for, request
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
@@ -38,6 +38,7 @@ def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
+    # validate user filled login data
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
@@ -49,9 +50,26 @@ def login():
         #     next_page = url_for('index')
         # return redirect(next_page)
         return redirect(url_for('index'))
+    # render login page for user to fill data
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    # validate user filled register data
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    # render register page for user to fill data
+    return render_template('register.html', title='Register', form=form)
