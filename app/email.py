ed8737274915adffb3b8fd0544310a34ae5a9cfc
bright <bright@gmail.com>
2019-03-12 23:46:50 +0800

    Change project name from faq_repo to FaqRepo.

--- app/email.py
+++ app/email.py
@@ -3,7 +3,7 @@
 
 import sys
 if sys.platform == 'darwin':
-    sys.path.insert(0, "/Users/chenhuming/workspace/faq_repo/venv/lib/python3.7/site-packages")
+    sys.path.insert(0, "/Users/chenhuming/workspace/FaqRepo/venv/lib/python3.7/site-packages")
 else:
     sys.path.insert(0, "/home/ehumche/private/py3-lib/lib/python3.5/site-packages")
 
@@ -27,7 +27,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('[Faq_repo] Reset Your Password',
+    send_email('[FaqRepo] Reset Your Password',
                sender=app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
